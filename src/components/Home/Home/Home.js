9718455f650968b9cadc29b5da3c4d3ede0994b3
Ratul <ratulkhan1428@gmail.com>
2021-04-11 18:04:45 +0600

    doctors portal home page done

---
+++ src/components/Home/Home/Home.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Blogs from '../Blogs/Blogs';
+import Contact from '../Contact/Contact';
+import Doctors from '../Doctors/Doctors';
+import FeaturedService from '../FeaturedService/FeaturedService';
+import Header from '../Header/Header';
+import MakeAppointment from '../MakeAppointment/MakeAppointment';
+import Services from '../Services/Services';
+import Testimonials from '../Testimonials/Testimonials';
+
+
+const Home = () => {
+    return (
+        <div>
+            <Header></Header>
+            <Services></Services>
+            <FeaturedService></FeaturedService>
+            <MakeAppointment></MakeAppointment>
+            <Testimonials></Testimonials>
+            <Blogs></Blogs>
+            <Doctors></Doctors>
+            <Contact></Contact>
+        </div>
+    );
+};
+
+export default Home;
